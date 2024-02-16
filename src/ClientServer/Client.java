143dc4827e5856dd92fa07b711f4f8fad0aeb264
ChandulaJ <chandugoods3000@gmail.com>
2024-02-16 10:29:51 +0530

    normal client server connected

--- src/ClientServer/Client.java
+++ src/ClientServer/Client.java
@@ -1,10 +1,37 @@
 package ClientServer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
+
+
 public class Client {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	static int port = 6600;
+	
+	public static void main(String[] args) throws IOException {
+		System.out.println("\t\t Simple client");
+		System.out.println("\t\t================\n\n");
+		
+		InetAddress ipAddress = InetAddress.getLocalHost(); // if it was between two computers, the ip address comes here
+		
+		Socket socket = new Socket(ipAddress,port);
+		System.out.println("Server is connected ...\n\n");
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		String input = in.readLine();
+		
+		System.out.println("Server says: "+ input);
+		
+		System.out.println("Closing connection ...");
+		
+		socket.close();
+		
+		in.close();
+		
+		
 	}
 
 }
--- src/ClientServer/Server.java
+++ src/ClientServer/Server.java
@@ -1,9 +1,36 @@
 package ClientServer;
 
-public class Server {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Server {
+	static int port = 6600;
+	public static void main(String[] args) throws IOException {
+		System.out.println("\t\t Simple Socket Server");
+		System.out.println("\t\t======================");
+
+		ServerSocket serversocket = new ServerSocket(port);
+		
+		try {
+		while(true) {
+		Socket socket = serversocket.accept();
+		System.out.println("Client is connected...\n\n");
+		
+		try {
+		PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+		out.println("Welcome to socket programming");
+		}
+		finally {
+			socket.close();
+		}
+		
+		}
+		}
+		finally {
+			serversocket.close();
+		}
 
 	}
 
